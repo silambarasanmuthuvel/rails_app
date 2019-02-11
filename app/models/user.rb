942f6fbbdc1ce63589fb6f7c0e3a870614463fca
silambarasan_mallow <silambarasansilambarasan007@gmail.com>
2019-02-11 16:26:42 +0530

    TAsk 16

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :comments
-  has_and_belongs_to_many :posts
+  has_many :posts , class_name: 'Post'
+  has_and_belongs_to_many :post
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
