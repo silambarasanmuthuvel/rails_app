8b3bf8442391306f4f8ac385d41f5bf2a5f8207c
silambarasan_mallow <silambarasansilambarasan007@gmail.com>
2019-02-11 12:25:45 +0530

    TASK 15 ajax call and marked the read statement

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   accepts_nested_attributes_for :tags
   belongs_to :user
   has_and_belongs_to_many :users
-  validates :title,presence:true ,length: { minimum: 3,maximum: 15,  message: "title should be of length 3 to 15 character" }
+  validates :title,presence:true ,length: { minimum: 3,maximum: 15,  message: "title should be of length 3 to 20 character" }
   validates :body,presence:true ,length: { minimum: 4,maximum: 100,  message: "body should be of length 4 to 100 character" }
 
   has_attached_file :image , styles: { medium: "300x300>", thumb: "100x100#" }
