8d7ff700f3c994e1e608a713a12c15334b8a0ba1
silambarasan_mallow <silambarasansilambarasan007@gmail.com>
2019-02-11 12:28:24 +0530

    task 16 created the alert and loading indicator

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
- 
+
   devise_for :users
   resources :tags
   resources :topics do
- 	resources :posts do
- 		resources :comments
-    resources :ratings
- 	end
-
+    resources :posts do
+      resources :comments
+      resources :ratings
+    end
   end
   get "/posts", to: 'posts#index'
-root "topics#index"
- get '/topics/:topic_id/posts/:id/readstatus' => 'posts#readstatus'
+  root "topics#index"
+  get '/topics/:topic_id/posts/:id/readstatus' => 'posts#readstatus'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/user", to: "application#authenticate"
+
 end
