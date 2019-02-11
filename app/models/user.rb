31b9a56f871958fc69020af1afe950a00c7401e6
silambarasan_mallow <silambarasansilambarasan007@gmail.com>
2019-02-11 19:16:08 +0530

    corrected the user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :comments
-  has_many :posts , class_name: 'Post'
+  has_many :created_posts , class_name: 'Post'
   has_and_belongs_to_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
